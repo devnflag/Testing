abc778606a0f0124611ef713a58da475a9a04ff4
Jonathan Urbina <jonathanurbina92@gmail.com>
2018-07-08 16:14:50 -0400

    Template

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+    include_once("class/db/db.php");
+
+    include_once("header/header.php");
+    include_once("middle/dashboard/dashboard.php");
+    include_once("footer/footer.php");
+?>
