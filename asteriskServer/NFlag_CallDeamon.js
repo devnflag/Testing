86bd35479bd5af26adeaf8dec32c6df39f86f257
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-07-08 19:59:15 -0400

    CallDaemon

---
+++ asteriskServer/NFlag_CallDeamon.js
@@ -0,0 +1,21 @@
+var aio = require('asterisk.io');
+var ami = null;
+
+ami = aio.ami('localhost',5038,'nflag','nflag.,2112');
+
+ami.on('error', function(err){
+    err = JSON.parse(JSON.stringify(err));
+    console.log(err);
+});
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 65530
+
+http.listen(port);
+require('events').EventEmitter.defaultMaxListeners = Infinity;
+
+ami.on('eventNewstate', function(data){
+    var evt = data;
+    console.log(evt);
+});
