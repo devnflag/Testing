af31dbad0db18ee156a870866b754030b9dec685
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-07-12 15:22:16 -0400

    asd

--- includes/AGI/db/DB.php
+++ includes/AGI/db/DB.php
@@ -1,10 +1,10 @@
 <?php
-    /* $Conf = "/var/www/html/class/db/conf.ini");
+    $Conf = parse_ini_file("/var/www/html/class/db/conf.ini");
     $Server = $Conf["serverDB"];
     $Pass = $Conf["passDB"];
     $User = $Conf["userDB"];
-    $Database = $Conf["DB"]; */
-    $Connection = new mysqli("localhost", "root", "nflag.,2112", "nflag");
+    $Database = $Conf["DB"];
+    $Connection = new mysqli($Server, $User, $Pass, $Database);
     if (mysqli_connect_errno()) {
         printf("Error de conexión: %s\n", mysqli_connect_error());
         exit();
