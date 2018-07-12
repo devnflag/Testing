73d9413fb63922819fd5d72d4b7ca3350cb4e686
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-07-12 15:35:23 -0400

    asd

--- includes/AGI/Telecom/getAccount.php
+++ includes/AGI/Telecom/getAccount.php
@@ -12,14 +12,16 @@
     $SqlTiempo = "SELECT DST.minutos as Minutos, (DST.minutos * 60) as Segundos FROM Extensiones Ex INNER JOIN usuarios Us on Us.id = Ex.idUsuario INNER JOIN data_sipTelecom DST on DST.idUsuario = Us.id WHERE Ex.Extension='".$Anexo."'";
     $Tiempo = $Connection->query($SqlTiempo);
     if(count($Tiempo) > 0){
-        $Minutos = $Tiempo[0]["Minutos"];
-        $Segundos = $Tiempo[0]["Segundos"];
-        $agi->set_variable(test,$Tiempo[0]["Segundos"]);
-        if($Segundos > 0){
-            $agi->set_variable(TimeOut,$Segundos);
-            $agi->exec_goto($Exten,2);
-        }else{
-            $agi->hangup();//ir a no tiene saldo disponible
+        foreach($Tiempo as $T){
+            $Minutos = $T["Minutos"];
+            $Segundos = $T["Segundos"];
+            $agi->set_variable(test,$T["Segundos"]);
+            if($Segundos > 0){
+                $agi->set_variable(TimeOut,$Segundos);
+                $agi->exec_goto($Exten,2);
+            }else{
+                $agi->hangup();//ir a no tiene saldo disponible
+            }
         }
     }else{
         $agi->hangup();
