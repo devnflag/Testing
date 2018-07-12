62c5c0359e5c703cf08a166a70f8712b3733f8a8
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-07-12 12:24:39 -0400

    asd

--- includes/AGI/Telecom/getAccount.php
+++ includes/AGI/Telecom/getAccount.php
@@ -5,6 +5,13 @@
 
     $agi = new AGI();
     $Anexo = $argv[1];
+    $Exten = $argv[2];
     $agi->set_variable(test,"1");
+
+    if($Anexo == "2001"){
+        $agi->exec_goto($exten,4);
+    }else{
+        $agi->hangup();
+    }
 
 ?>
