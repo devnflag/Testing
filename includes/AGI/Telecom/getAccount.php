775967e526fa90473c5259c7cc5783f1f147959e
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-07-23 10:42:40 -0400

    asd

--- includes/AGI/Telecom/getAccount.php
+++ includes/AGI/Telecom/getAccount.php
@@ -9,22 +9,48 @@
     $Anexo = trim($argv[1]);
     $Exten = $argv[2];
     
-    $SqlTiempo = "SELECT DST.minutos as Minutos, (DST.minutos * 60) as Segundos FROM Extensiones Ex INNER JOIN usuarios Us on Us.id = Ex.idUsuario INNER JOIN data_sipTelecom DST on DST.idUsuario = Us.id WHERE Ex.Extension='".$Anexo."'";
+    $NoSaldo = false;
+    
+    $SqlTiempo = "SELECT DST.minutos as Minutos, (DST.minutos * 60) as Segundos, DST.saldo as Saldo FROM Extensiones Ex INNER JOIN usuarios Us on Us.id = Ex.idUsuario INNER JOIN data_sipTelecom DST on DST.idUsuario = Us.id WHERE Ex.Extension='".$Anexo."'";
     $Tiempo = $Connection->query($SqlTiempo);
     if(count($Tiempo) > 0){
         foreach($Tiempo as $T){
+            $Saldo = $T["Saldo"];
             $Minutos = $T["Minutos"];
-            $Segundos = round($T["Segundos"]);
-            $agi->set_variable(test,$T["Segundos"]);
-            if($Segundos > 0){
-                $agi->set_variable(TimeOut,$Segundos);
-                /* $agi->exec_goto($Exten,2); */
+            if($Minutos > 0){
+                $Segundos = round($T["Segundos"]);
+                $agi->set_variable(test,$Segundos);
+                if($Segundos > 0){
+                    $agi->set_variable(TimeOut,$Segundos);
+                    /* $agi->exec_goto($Exten,2); */
+                }else{
+                    $NoSaldo = true;
+                }
             }else{
-                $agi->hangup();//ir a no tiene saldo disponible
+                $SqlPrecioMinutoUnitario = "SELECT precioUnitarioMinuto as Precio FROM config_sipTelecom";
+                $PrecioMinutoUnitario = $Connection->query($SqlPrecioMinutoUnitario);
+                $PrecioMinutoUnitario = $PrecioMinutoUnitario[0];
+                $PrecioMinuto = $PrecioMinutoUnitario["Precio"];
+                if($Saldo >= $PrecioMinuto){
+                    $Minutos = $Saldo / $PrecioMinuto;
+                    $Segundos = round($Minutos * 60);
+                    $agi->set_variable(test,$Segundos);
+                    if($Segundos > 0){
+                        $agi->set_variable(TimeOut,$Segundos);
+                        /* $agi->exec_goto($Exten,2); */
+                    }else{
+                        $NoSaldo = true;
+                    }
+                }else{
+                    $NoSaldo = true;
+                }
             }
         }
     }else{
-        $agi->hangup();
+        $NoSaldo = true;
+    }
+    if($NoSaldo){
+        $agi->hangup();//ir a no tiene saldo disponible
     }
 
 ?>
