9acf127587ee5bbbef879723de1b3cb99d169ecd
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-07-25 14:38:05 -0400

    asd

--- header/headerIndex.php
+++ header/headerIndex.php
@@ -365,7 +365,7 @@
                         </div>
 
                         <div class="dropdown-menu">
-                        <a class="dropdown-item" href="cuenta/perfil.php">Perfil</a>
+                            <!-- <a class="dropdown-item" href="cuenta/perfil.php">Perfil</a> -->
                             <a class="dropdown-item" href="logout.php">Salir</a>
                         </div>
                     </div>
--- middle/cuenta/perfil.php
+++ middle/cuenta/perfil.php
@@ -3,96 +3,37 @@
                     <small>Welcome to the unique SuperAdmin web app experience!</small>
                 </header>
                 <div class="card new-contact">
-                        <div class="new-contact__header">
-                            <a href="" class="zmdi zmdi-camera new-contact__upload"></a>
+                    <div class="card-body">
+                        <h4 class="card-title">Registro de Usuario</h4>
 
-                            <img src="../demo/img/contacts/user_empty.png" class="new-contact__img" alt="">
+                        <div class="form-group">
+                            <h6>Ingrese su nombre completo</h6>
+                            <input type="text" name="name" class="form-control form-control-lg">
+                            <i class="form-group__bar"></i>
                         </div>
-
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>First Name</label>
-                                        <input type="text" class="form-control" placeholder="i.e. John">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Last Name</label>
-                                        <input type="text" class="form-control" placeholder="i.e. Smith">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Company</label>
-                                        <input type="text" class="form-control" placeholder="i.e. Google Inc.">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Job Title</label>
-                                        <input type="text" class="form-control" placeholder="i.e. Web Developer">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Email Address</label>
-                                        <input type="text" class="form-control" placeholder="i.e. smith@gmail.com">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Website</label>
-                                        <input type="text" class="form-control" placeholder="i.e. http://smith.com">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Mobile Phone</label>
-                                        <input type="text" class="form-control" placeholder="i.e. +14155552671">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Home Phone</label>
-                                        <input type="text" class="form-control" placeholder="i.e. +415 555 2671">
-                                        <i class="form-group__bar"></i>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Address</label>
-                                <textarea class="form-control textarea-autosize" placeholder="i.e. 711-2880 Nulla St. Mankato, Mississippi, 96522." style="overflow: hidden; word-wrap: break-word; height: 48px;"></textarea>
-                                <i class="form-group__bar"></i>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Notes</label>
-                                <textarea class="form-control textarea-autosize" style="overflow: hidden; word-wrap: break-word; height: 48px;"></textarea>
-                                <i class="form-group__bar"></i>
-                            </div>
-
-                            <div class="clearfix"></div>
-
-                            <div class="mt-5 text-center">
-                                <a href="" class="btn btn-light">Save new contact</a>
-                                <a href="" class="btn btn-light">Clear</a>
-                            </div>
+                        <div class="form-group">
+                            <h6>Ingese su DNI</h6>
+                            <input type="text" name="dni" class="form-control form-control-lg">
+                            <i class="form-group__bar"></i>
                         </div>
+                        <div class="form-group">
+                            <h6>Ingrese su correo electrónico</h6>
+                            <input type="text" name="mail" class="form-control form-control-lg">
+                            <i class="form-group__bar"></i>
+                        </div>
+                        <div class="form-group">
+                            <h6>Vuelva a ingresar su correo electronico</h6>
+                            <input type="text" name="remail" class="form-control form-control-lg">
+                            <i class="form-group__bar"></i>
+                        </div>
+                        <div class="form-group">
+                            <h6>Ingrese su dirección</h6>
+                            <textarea name="address" class="form-control" rows="5"></textarea>
+                            <i class="form-group__bar"></i>
+                        </div>
+                        
+                        <button type="button" name="ok" class="btn btn-primary">Actualizar</button>
+
                     </div>
+                </div>
+            </div>
