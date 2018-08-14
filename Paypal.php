e0b6eeca9d474d3a4d8577954a03915a44ae45c7
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-08-14 16:39:20 -0300

    asdasdasdasd

--- Paypal.php
+++ Paypal.php
@@ -1,52 +1,52 @@
 <?php
     include_once("class/PayPal/DPayPal.php");
-$paypal = new DPayPal(); //Create an object
+    $paypal = new DPayPal(); //Create an object
 
-//Generating request parameters for API operation SetExpressCheckout
-//All available parameters for this method are available here
-//https://developer.paypal.com/docs/classic/api/merchant/SetExpressCheckout_API_Operation_NVP/
+    //Generating request parameters for API operation SetExpressCheckout
+    //All available parameters for this method are available here
+    //https://developer.paypal.com/docs/classic/api/merchant/SetExpressCheckout_API_Operation_NVP/
 
-$requestParams = array(
-    'RETURNURL' => "http://app.nflag.io/includes/cuenta/recarga_paypal.php", //Enter your webiste URL here
-    'CANCELURL' => "http://app.nflag.io/includes/cuenta/recarga_paypal.php"//Enter your website URL here
-);
+    $requestParams = array(
+        'RETURNURL' => "http://app.nflag.io/includes/cuenta/recarga_paypal.php", //Enter your webiste URL here
+        'CANCELURL' => "http://app.nflag.io/includes/cuenta/recarga_paypal.php"//Enter your website URL here
+    );
 
-$orderParams = array(
-    'LOGOIMG' => "", //You can paste here your website logo image which will be displayed to the customer on the PayPal chechout page
-    "MAXAMT" => "100", //Set max transaction amount
-    "NOSHIPPING" => "1", //I do not want shipping
-    "ALLOWNOTE" => "0", //I do not want to allow notes
-    "BRANDNAME" => "Here enter your brand name",
-    "GIFTRECEIPTENABLE" => "0",
-    "GIFTMESSAGEENABLE" => "0"
-);
-$item = array(
-    'PAYMENTREQUEST_0_AMT' => "0.01",
-    'PAYMENTREQUEST_0_CURRENCYCODE' => 'USD',
-    'PAYMENTREQUEST_0_ITEMAMT' => "0.01",
-    'L_PAYMENTREQUEST_0_NAME0' => 'Item name',
-    'L_PAYMENTREQUEST_0_DESC0' => 'Item description',
-    'L_PAYMENTREQUEST_0_AMT0' => "0.01",
-    'L_PAYMENTREQUEST_0_QTY0' => '1',
-        //"PAYMENTREQUEST_0_INVNUM" => $transaction->id - This field is useful if you want to send your internal transaction ID
-);
+    $orderParams = array(
+        'LOGOIMG' => "", //You can paste here your website logo image which will be displayed to the customer on the PayPal chechout page
+        "MAXAMT" => "100", //Set max transaction amount
+        "NOSHIPPING" => "1", //I do not want shipping
+        "ALLOWNOTE" => "0", //I do not want to allow notes
+        "BRANDNAME" => "Here enter your brand name",
+        "GIFTRECEIPTENABLE" => "0",
+        "GIFTMESSAGEENABLE" => "0"
+    );
+    $item = array(
+        'PAYMENTREQUEST_0_AMT' => "0.01",
+        'PAYMENTREQUEST_0_CURRENCYCODE' => 'USD',
+        'PAYMENTREQUEST_0_ITEMAMT' => "0.01",
+        'L_PAYMENTREQUEST_0_NAME0' => 'Item name',
+        'L_PAYMENTREQUEST_0_DESC0' => 'Item description',
+        'L_PAYMENTREQUEST_0_AMT0' => "0.01",
+        'L_PAYMENTREQUEST_0_QTY0' => '1',
+            //"PAYMENTREQUEST_0_INVNUM" => $transaction->id - This field is useful if you want to send your internal transaction ID
+    );
 
- //Send request and wait for response
- //Now we will call SetExpressCheckout API operation. 
+    //Send request and wait for response
+    //Now we will call SetExpressCheckout API operation. 
 
-$response = $paypal->SetExpressCheckout($requestParams + $orderParams + $item);
+    $response = $paypal->SetExpressCheckout($requestParams + $orderParams + $item);
 
-//Response is aslo accessible by calling  $paypal->getLastServerResponse()
+    //Response is aslo accessible by calling  $paypal->getLastServerResponse()
 
-//Now you will be redirected to the PayPal to enter your customer data
-//After that, you will be returned to the RETURN URL
-if (is_array($response) && $response['ACK'] == 'Success') { //Request successful
-    //Now we have to redirect user to the PayPal
-    $token = $response['TOKEN'];
+    //Now you will be redirected to the PayPal to enter your customer data
+    //After that, you will be returned to the RETURN URL
+    if (is_array($response) && $response['ACK'] == 'Success') { //Request successful
+        //Now we have to redirect user to the PayPal
+        $token = $response['TOKEN'];
 
-    header('Location: https://www.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=' . urlencode($token));
-} else if (is_array($response) && $response['ACK'] == 'Failure') {
-    var_dump($response);
-    exit;
-}
+        header('Location: https://www.paypal.com/cgi-bin/webscr?cmd=_express-checkout&saldo=2500&C='.$_SESSION["idCliente"].'&token=' . urlencode($token));
+    } else if (is_array($response) && $response['ACK'] == 'Failure') {
+        var_dump($response);
+        exit;
+    }
 ?>
--- includes/cuenta/recarga_paypal.php
+++ includes/cuenta/recarga_paypal.php
@@ -9,19 +9,22 @@
 	
 	$response = $paypal->GetExpressCheckoutDetails($requestParams);
 	$payerId=$response["PAYERID"];//Payer id returned by paypal
-	print_r($response);
+    echo "<pre>";
+    print_r($response);
+    echo "</pre>";
 	//Create request for DoExpressCheckoutPayment
 	$requestParams=array(
 		"TOKEN"=>$token,
 		"PAYERID"=>$payerId,
-		"PAYMENTREQUEST_0_AMT"=>"0.01",//Payment amount. This value should be sum of of item values, if there are more items in order
-		"PAYMENTREQUEST_0_CURRENCYCODE"=>"USD",//Payment currency
-		"PAYMENTREQUEST_0_ITEMAMT"=>"0.01"//Item amount
+		"PAYMENTREQUEST_0_AMT"=>$response["AMT"],//Payment amount. This value should be sum of of item values, if there are more items in order
+		"PAYMENTREQUEST_0_CURRENCYCODE"=>$response["CURRENCYCODE"],//Payment currency
+		"PAYMENTREQUEST_0_ITEMAMT"=>$response["ITEMAMT"]//Item amount
 	);
 	$transactionResponse=$paypal->DoExpressCheckoutPayment($requestParams);//Execute transaction
 	
 	if(is_array($transactionResponse) && $transactionResponse["ACK"]=="Success"){//Payment was successfull
-		//Successful Payment
+        //Successful Payment
+        echo "PAGO REALIZADO CORRECTAMENTE";
 	}
 	else{
 		//Failure
