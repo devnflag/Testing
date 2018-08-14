f8f82ce1c898989b99980a6017ab580e01e2aa15
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2018-08-14 17:24:00 -0300

    asda

--- includes/cuenta/recarga_paypal.php
+++ includes/cuenta/recarga_paypal.php
@@ -35,7 +35,7 @@
             $SqlInsertRecarga = "insert into recargas (idComprobante,fechaRecarga,montoRecarga) values ('".$idComprobante."',NOW(),'".$_GET["saldo"]."')";
             $InsertRecarga = $db->query($SqlInsertRecarga);
         }
-        header('Location: http://app.nflag.io')
+        header('Location: http://app.nflag.io');
 	}
 	else{
 		//Failure
