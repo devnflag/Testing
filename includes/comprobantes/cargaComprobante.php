78d13728cfac1b311b52bc42a121da97f891159f
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2019-01-11 10:55:00 -0300

    asd

--- includes/comprobantes/cargaComprobante.php
+++ includes/comprobantes/cargaComprobante.php
@@ -33,6 +33,7 @@
             shell_exec("chmod 777 ".$ficheroFinal);
             $ToReturn["result"] = true;
             $MailClass = new Mail();
+            echo $ficheroFinal;
             $MailClass->sendMailNewComprobante($ficheroFinal,$tipoComprobante);
         }else{
             $ToReturn["Message"] = "Hubo un error al subir el archivo.";
