309b52ad524e7a7ed06b1a105eeb2106236bcb0e
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2019-01-11 11:06:57 -0300

    das

--- class/mail/mail.php
+++ class/mail/mail.php
@@ -1165,7 +1165,7 @@
 			</html>';
 			return $ToReturn;
 		}
-		function newComprobanteContentMail($ficheroFinal,$tipoComprobante){
+		function newComprobanteContentMail($ficheroFinal,$tipoComprobante,$idCliente,$idComprobante){
 			$ToReturn = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 			"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
