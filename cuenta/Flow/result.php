faf15c5d5f978c587a1bd363ace385aff8b394b8
jonathanurbina92@gmail.com <jonathanurbina92@gmail.com>
2019-01-14 17:37:31 -0300

    dasd

--- cuenta/Flow/result.php
+++ cuenta/Flow/result.php
@@ -6,6 +6,9 @@
      * mostrar su propio comprobante de pago
      */
     require("../../class/Flow/FlowApi.class.php");
+    include_once("../../class/db/db.php");
+    $db = new DB();
+
     try {
         //Recibe el token enviado por Flow
         if(!isset($_POST["token"])) {
@@ -20,8 +23,22 @@
         $flowApi = new FlowApi();
         $response = $flowApi->send($serviceName, $params, "GET");
         
-        print_r($response);
-        
+        $Status = $response["status"];
+        $idComprobante = $response["commerceOrder"];
+        $Saldo = $response["amount"];
+        switch($Status){
+            case 1: //Pending
+            break;
+            case 2: //Success
+                $SqlUpdateComprobante = "update comprobantes set status='1' where id='".$idComprobante."'";
+                $InsertComprobante = $db->query($SqlInsertComprobante);
+            break;
+            case 3: //Declined
+                $SqlUpdateComprobante = "update comprobantes set status='3' where id='".$idComprobante."'";
+                $InsertComprobante = $db->query($SqlInsertComprobante);
+            break;
+        }
+        header('Location: http://app.nflag.io');
     } catch (Exception $e) {
         echo "Error: " . $e->getCode() . " - " . $e->getMessage();
     }
